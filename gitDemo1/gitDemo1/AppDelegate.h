6ae73c734b4395f6a959b7013bc57d4d1bcb2a4b
wufei <jay4900@163.com>
2014-02-21 10:07:43 +0800

    create

---
+++ gitDemo1/gitDemo1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitDemo1
+//
+//  Created by wufei on 14-2-21.
+//  Copyright (c) 2014年 wufei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitDemo1/gitDemo1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitDemo1
+//
+//  Created by wufei on 14-2-21.
+//  Copyright (c) 2014年 wufei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
